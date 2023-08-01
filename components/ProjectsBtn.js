5189ad162bc1bcede52b5c3fa412f5c9a32668b8
Carla0106 <ca.sossa@duocuc.cl>
2023-08-01 19:00:10 -0400

    holaa

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -12,18 +12,18 @@ const ProjectsBtn = () => {
     <div className='mx-auto xl:mx-0'>
       <Link 
         href={'/work'}
-        className='relative w-[185px] h-[185px] flex justify-center items-center
+        className='relative w-[150px] h-[150px] flex justify-center items-center
         bg-circleStar bg-cover bg-center bg-no-repeat group '
       >
         <Image
-        src={'/rounded-text.png'}
-        width={141}
-        height={148}
-        alt=''
-        className='animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]'
-      />
-      <HiArrowRight className='absolute text-4xl group-hover:translate-x-2
-      transition-all duration-300'/>
+          src={'/rounded-text.png'}
+          width={103}
+          height={115}
+          alt=''
+          className='animate-spin-slow w-full h-full max-w-[103px] max-h-[115px]'
+        />
+        <HiArrowRight className='absolute text-1xl group-hover:translate-x-2
+        transition-all duration-300'/>
       </Link>
     </div>
   ) ;
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,34 @@
 // next image
 import Image from "next/image";
 
-//components
+// components
 import ParticlesContainer from '../components/ParticlesContainer';
 import ProjectsBtn from '../components/ProjectsBtn';
 import Avatar from '../components/Avatar';
 
-//framer motion
+// framer motion
 import { motion } from "framer-motion";
 
-//variants
+// variants
 import { fadeIn } from '../variants';
 
 const Home = () => {
   return ( 
     <div className='bg-primary/60 h-full'>
-      {/* text*/ }
-      <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30
-      to-black/10'>
-        <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left
-        h-full container mx-auto'>
-          {/*tittle*/}
+      {/* text */}
+      <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
+        <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
+          {/* title */}
           <motion.h1 
             variants={fadeIn('down', 0.2)}
             initial='hidden'
             animate='show'
             exit='hidden'
             className='hi'
+            style={{ margin: '0 auto 0 6rem' }}
           >
-            Transforming ideas <br /> into {' '}
-            <span className= 'text-accent'>Digital Reality</span>
+            Transforming ideas <br /> Into {' '}
+            <span className='text-accent'>Digital Reality</span>
           </motion.h1>
           {/* subtitle */}
           <motion.p 
@@ -38,12 +37,13 @@ const Home = () => {
             animate='show'
             exit='hidden'
             className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'
+            style={{ margin: '0 auto 0 6rem' }}
           >
             lorem ipsum dolor sit amet 
           </motion.p>
           {/* btn */}
           <div className='flex justify-center xl:hidden relative'>
-            <ProjectsBtn/>
+            <ProjectsBtn />
           </div>
           <motion.div 
             variants={fadeIn('down', 0.4)}
@@ -51,13 +51,32 @@ const Home = () => {
             animate='show'
             exit='hidden'
             className='hidden xl:flex'
+            style={{ margin: '0 auto 0 6rem' }}
           >
             <ProjectsBtn />
           </motion.div>
+        </div> 
+        {/* image */}
+        <div className='w-[1200px] h-full absolute right-0 bottom-0'>
+          {/* bg img */}
+          <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'></div>
+          {/* particles */}
+          <ParticlesContainer />
+          {/* avatar img */}
+          <div className='w-full h-full flex justify-center items-center'>
+            <motion.div
+              variants={fadeIn('up', 0.5)}
+              initial='hidden'
+              animate='show'
+              exit='hidden'
+              transition={{duration: 1, ease: 'easeInOut'}}
+              className='w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]'
+            >
+              <Avatar />
+            </motion.div>
+          </div>
         </div>
-      </div> 
-      {/*image*/}
-      <div>image</div>
+      </div>
     </div>
   );
 };
